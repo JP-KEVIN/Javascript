44d0c05dcac55b7ab5dca22e25d15786e5276747
yinismith <yinierm24@gmail.com>
2022-07-19 09:13:04 -0500

    este es java

---
+++ Clase 2/scripts/app.js
@@ -0,0 +1,14 @@
+let firstName="Kevin";
+let lastName="Jaramillo";
+let gender="female";
+//let age=18
+var age =5;
+if (gender=="male") {
+ 
+}else{
+    console.log(age)
+}
+
+firstName="pepe"
+
+console.log(`Bienvenido ${firstName} ${lastName} su genero es: ${gender} su edad es: ${age}`);
